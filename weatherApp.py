4c76bebbdc50ad50d40103e017be34256301937b
Aidan <aidan.yun@outlook.com>
2025-10-18 00:11:14 -0700

    weather app

---
+++ weatherApp.py
@@ -0,0 +1,104 @@
+import sys
+import requests
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
+                            QLineEdit, QPushButton, QVBoxLayout)
+
+from PyQt5.QtCore import Qt
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter city name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel(self)
+        self.emoji_label = QLabel(self)
+        self.description_label = QLabel(self)
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle("Weather App")
+        
+        vbox = QVBoxLayout()
+        
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+        
+        self.setLayout(vbox)
+        
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+        
+        self.city_label.setObjectName("city_label")
+        self.city_input.setObjectName("city_input")
+        self.get_weather_button.setObjectName("get_weather_button")
+        self.temperature_label.setObjectName("temperature_label")
+        self.emoji_label.setObjectName("emoji_label")
+        self.description_label.setObjectName("description_label")
+        
+        self.setStyleSheet("""
+            QLabel, QPushButton {
+                font-family: calibri;
+            }
+            QLabel#city_label {
+                font-size: 40px;
+                font-style: italic;
+            }
+            QLineEdit#city_input {
+                font-size: 40px;
+            }
+            QPushButton#get_weather_button {
+                font-size: 30px;
+                font-weight: bold;
+            }
+            QLabel#temperature_label {
+                font-size: 75px;
+            }
+            QLabel#emoji_label {
+                font-size: 100px;
+                font-family: Segoe UI emoji;
+            }
+            QLabel#description_label {
+                font-size: 50px;
+                
+            }   
+        """)
+        
+        self.get_weather_button.clicked.connect(self.get_weather)
+
+    def get_weather(self):
+        api_key = os.getenv("API_KEY")
+        
+        city = self.city_input.text()
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+        
+        response = requests.get(url)
+        data = response.json()
+        
+        print(data)
+    
+    def display_error(self, message):
+        pass
+    
+    def display_weather(self, data):
+        pass
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
+    
+    
